c9ca3db94a9342a5e1aea92acaa1c4de56641a3f
Andrei Eres <andrei.eres@ya.ru>
2021-06-06 22:25:42 +0300

    Stub creating offers from external api

---
+++ app/services/create_offers_from_external_api_service.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'external_api'
+
+class CreateOffersFromExternalApiService
+  def call
+    # Some logic to fetch and create offers using data
+    # from https://bravado.co/api/api/opportunity/intros.json
+    #
+    # In case the test task we assume we are able to syncronize data
+    # with external api by periodical background jobs
+  end
+end
