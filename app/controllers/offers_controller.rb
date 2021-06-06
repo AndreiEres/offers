8cc8381044e2e697f33936b020121ca8a209e822
Andrei Eres <andrei.eres@ya.ru>
2021-06-06 15:01:41 +0300

    Update offers controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,6 +2,18 @@
 
 class OffersController < ApplicationController
   def index
-    render json: Offer.all
+    ids = OfferIdsForUserService.new.call(user: user)
+    pagy, records = pagy(Offer.by_ids(ids[:all]))
+
+    render json: {
+      data: records.map { |r| OfferSerializer.new(r, ids).as_json },
+      **pagy_metadata(pagy)
+    }
+  end
+
+  private
+
+  def user
+    User.find_by(id: params[:user_id])
   end
 end
--- spec/requests/offers_spec.rb
+++ spec/requests/offers_spec.rb
@@ -4,18 +4,43 @@ require 'rails_helper'
 
 RSpec.describe 'offers', type: :request do
   describe 'GET /' do
-    it 'returns status ok' do
-      get '/offers'
+    let(:result) do
+      {
+        'count' => 3,
+        'data' => [
+          { 'company' => 'Apple', 'id' => 999, 'label' => 'perfect_match', 'price' => 999 },
+          { 'company' => 'Apple', 'id' => 212, 'label' => 'good_match', 'price' => 212 },
+          { 'company' => 'Google', 'id' => 500, 'label' => 'other', 'price' => 500 }
+        ],
+        'items' => 3,
+        'page' => 1,
+        'pages' => 1
 
-      expect(response).to have_http_status(:ok)
+      }
     end
 
-    it 'returns all offers' do
-      offers = create_list(:offer, 3)
+    before do
+      create(:user, id: 100, company: 'Apple').tap do |user|
+        user.departments << create(:department, name: 'IT')
+      end
 
-      get '/offers'
+      create(:offer, id: 999, company: 'Apple', price: 999).tap do |perfect_match|
+        perfect_match.departments << create(:department, name: 'IT')
+      end
 
-      expect(json_response).to eq(offers.map(&:as_json))
+      create(:offer, id: 212, company: 'Apple', price: 212).tap do |good_match|
+        good_match.departments << create(:department, name: 'Accounting')
+      end
+
+      create(:offer, id: 500, company: 'Google', price: 500).tap do |good_match|
+        good_match.departments << create(:department, name: 'Accounting')
+      end
+    end
+
+    it 'returns offers' do
+      get '/offers', params: { user_id: 100 }
+
+      expect(json_response).to eq(result)
     end
   end
 end
