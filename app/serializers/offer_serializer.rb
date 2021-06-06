a69e308265bcad0929a38f95b9d588b5849147c0
Andrei Eres <andrei.eres@ya.ru>
2021-06-06 13:52:21 +0300

    Add label to offer serializer

--- app/serializers/offer_serializer.rb
+++ app/serializers/offer_serializer.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 class OfferSerializer
-  attr_reader :instance
+  attr_reader :instance, :params
 
-  def initialize(instance)
+  def initialize(instance, params = {})
     @instance = instance
+    @params = params
   end
 
   def as_json
-    instance.as_json(only: %i[id price company])
+    instance
+      .as_json(only: %i[id price company])
+      .merge('label' => label)
+  end
+
+  private
+
+  def label
+    OfferLabel.new(id: instance.id, ids: params).to_s
   end
 end
--- spec/serializers/offer_serializer_spec.rb
+++ spec/serializers/offer_serializer_spec.rb
@@ -4,13 +4,23 @@ require 'rails_helper'
 
 RSpec.describe OfferSerializer do
   describe '#as_json' do
-    subject { described_class.new(offer).as_json }
+    subject { described_class.new(offer, params).as_json }
 
     let(:offer) { build(:offer, id: 1, price: 100, company: 'Apple') }
+    let(:params) { {} }
     let(:hash) do
-      { 'id' => 1, 'price' => 100, 'company' => 'Apple' }
+      { 'id' => 1, 'price' => 100, 'company' => 'Apple', 'label' => '' }
     end
 
     it { is_expected.to eq(hash) }
+
+    context 'when params for label passed' do
+      let(:params) { { label: [1] } }
+      let(:hash) do
+        { 'id' => 1, 'price' => 100, 'company' => 'Apple', 'label' => 'label' }
+      end
+
+      it { is_expected.to eq(hash) }
+    end
   end
 end
